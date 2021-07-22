d6637d0c75f7fef9a8a8a1fd4c7c8f2d2db3aa06
minssan9 <minssan9@gmail.com>
2021-07-22 19:58:02 +0900

    winformExcelChart init

---
+++ WinFormsExcelToChart/WinFormsExcelToChart/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace WinFormsExcelToChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openExcelFile = new System.Windows.Forms.Button();
+            this.dataGridExcel = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridExcel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(349, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(528, 628);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openExcelFile
+            // 
+            this.openExcelFile.Location = new System.Drawing.Point(12, 22);
+            this.openExcelFile.Name = "openExcelFile";
+            this.openExcelFile.Size = new System.Drawing.Size(156, 23);
+            this.openExcelFile.TabIndex = 1;
+            this.openExcelFile.Text = "Open Excel File";
+            this.openExcelFile.UseVisualStyleBackColor = true;
+            this.openExcelFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridExcel
+            // 
+            this.dataGridExcel.AllowUserToOrderColumns = true;
+            this.dataGridExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridExcel.Location = new System.Drawing.Point(12, 280);
+            this.dataGridExcel.Name = "dataGridExcel";
+            this.dataGridExcel.RowTemplate.Height = 23;
+            this.dataGridExcel.Size = new System.Drawing.Size(240, 150);
+            this.dataGridExcel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 645);
+            this.Controls.Add(this.dataGridExcel);
+            this.Controls.Add(this.openExcelFile);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridExcel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openExcelFile;
+        private System.Windows.Forms.DataGridView dataGridExcel;
+    }
+}
+
---
+++ WinFormsExcelToChart/WinFormsExcelToChart/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsExcelToChart
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog oFileDialog = new OpenFileDialog();
+        List<DataTable> dataTableList = new List<DataTable>();
+        BindingSource bindSource = new BindingSource();
+        List<String> fileNames = new List<String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oFileDialog.Filter = "data file (*.csv)|*.csv;Excel (*.xlsx)|*.xlsx|*.xls";
+            oFileDialog.Multiselect = true;
+
+            oFileDialog.ShowDialog();
+            foreach (string fileName in oFileDialog.FileNames)
+            {
+                fileNames.Add(fileName);
+                dataTableList.Add(ConvertCSVtoDataTable(fileName));
+            }
+
+            DataTable resultDt = new DataTable();
+            resultDt = getDataTableFromFiles(dataTableList);
+            dataGridExcel.DataSource = resultDt;
+        }
+        //private void btnDrawChart_Click(object sender, RoutedEventArgs e)
+        //{
+        //    DataTable dtChart = new DataTable();
+        //    gridChart(dtChart);
+        //}
+
+
+        public static DataTable getDataTableFromFiles(List<DataTable> dataTables)
+        {
+            DataTable resultDt = new DataTable();
+            foreach (DataTable dataTable in dataTables)
+            {
+                DataColumn dataColumn = new DataColumn();
+                dataColumn.ColumnName = dataTable.TableName;
+                foreach (DataRow row in dataTable.Columns[2].Table.Rows)
+                {
+                    dataColumn.Table.Rows.Add(row);
+                }                
+                resultDt.Columns.Add(dataColumn);
+            }
+
+            return resultDt;
+        }
+        private void gridChart(DataTable dataTable)
+        {
+
+        }
+
+        public static DataTable ConvertCSVtoDataTable(string strFilePath)
+        {
+            DataTable dt = new DataTable();
+            using (StreamReader sr = new StreamReader(strFilePath))
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    sr.ReadLine();
+                }
+
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    if (rows[0].Equals("") || rows[0].Contains("Event:20:time")) break;
+
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {                        
+                        dr[i] = rows[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            dt.TableName =strFilePath;
+            return dt;
+        }
+
+    }
+}  
